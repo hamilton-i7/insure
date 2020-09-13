ce580e7191390ea38bdca0e0e634c4e078efbb6e
Juan Hamilton <hedwards507@gmail.com>
2020-09-13 01:50:01 -0500

    added mobile na

---
+++ root/js/script.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+const menuIcon = document.querySelector(".js--menuIcon");
+const linksContainer = document.querySelector(".js--linksContainer");
+
+
+menuIcon.addEventListener("click", () => {
+    const hamburger = "hamburger.svg";
+    const close = "close.svg";
+    
+    if (menuIcon.src.endsWith("hamburger.svg")) {
+        menuIcon.setAttribute("src", `images/icon-${close}`);
+        linksContainer.classList.remove("animate-out");
+        linksContainer.classList.remove("hide");
+        linksContainer.classList.add("animate");
+        body.classList.add("stop-scroll");
+    } else {
+        menuIcon.setAttribute("src", `images/icon-${hamburger}`);
+        linksContainer.classList.remove("animate");
+        linksContainer.classList.add("animate-out");
+        body.classList.remove("stop-scroll");
+    }
+});
